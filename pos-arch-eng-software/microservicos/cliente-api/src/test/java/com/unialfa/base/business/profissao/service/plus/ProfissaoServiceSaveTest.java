7a7ccd7bda57de4ab3244c3dcbb46c12ea48f114
luisgustavo <luis.gustav0@live.com>
2019-11-20 00:56:19 -0300

    Primeiro commit

--- pos-arch-eng-software/microservicos/cliente-api/src/test/java/com/unialfa/base/business/profissao/service/plus/ProfissaoServiceSaveTest.java
+++ pos-arch-eng-software/microservicos/cliente-api/src/test/java/com/unialfa/base/business/profissao/service/plus/ProfissaoServiceSaveTest.java
@@ -2,12 +2,21 @@ package com.unialfa.base.business.profissao.service.plus;
 
 import com.unialfa.base.business.profissao.Profissao;
 import com.unialfa.base.business.profissao.service.ProfissaoService;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ProfissaoServiceSaveTest {
 
-    @Mock
+    @Autowired
     private ProfissaoService profissaoService;
 
     private Profissao getProfissao(String descricao) {
@@ -18,7 +27,18 @@ public class ProfissaoServiceSaveTest {
     }
 
     @Test
-    public void saveTest() {
+    public void saveSuccessTest() {
+        System.out.println("dasdasd");
+        Profissao profissao = getProfissao("Full stack developer");
+
+        profissao = this.profissaoService.save(profissao);
+
+        assertThat(profissao).isNotNull();
+        assertThat(profissao.getId()).isNotNull();
+    }
+
+    @Test(expected = ConstraintViolationException.class)
+    public void saveNullTest() {
         Profissao profissao = getProfissao(null);
 
         this.profissaoService.save(profissao);
